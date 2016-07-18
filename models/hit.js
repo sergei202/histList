d2f846c3e9b4b1158ac3ca87a18b90dacab458d7
Sergei <sergei@sleekbook>
2016-07-17 20:54:15 -0500

    Add hit bids.

--- models/hit.js
+++ models/hit.js
@@ -5,5 +5,10 @@ module.exports = mongoose.model('Hit', {									// Create a model called Hit an
 	bounty: Number,
 	location: String,
 	contractor: {type:mongoose.Schema.Types.ObjectId, ref:'Contractor'},	// contractor is an ObjectId that references the Contractor model
-	status: String
+	status: String,
+	bids: [{
+		name: String,			// The bidder's name
+		amount: Number,			// The bid amount
+		days: Number			// Number of days to complete the job
+	}]
 });
